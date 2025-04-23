be76b79fbc4b5d5d03280c321f43042300d27788
MayerlinLemosBedoya <maleiquianchico@hotmail.com>
2025-04-23 11:15:52 -0500

    subiendo

---
+++ ejemplos/main.js
@@ -0,0 +1,24 @@
+// let factorial=1
+// for(let i=1; i<=5;i=i+1){
+//     factorial=factorial*i
+//     console.log("iteraction",i)
+// }
+// console.log(factorial)
+
+// let j =0;
+// do {
+//     console.log(j);
+//     j++;
+// }while (j < 5);
+
+let frutas=["manzana","banana","pera"];
+
+console.log(frutas[0]); //"manzana"
+console.log(frutas[2]); //"pera"
+
+frutas[1]="kiwi";
+console.log(frutas);//["manzana","kiwi","pera"]
+console.log(frutas.length)
+for (let i=0; i<frutas.length;i++){
+    console.log(frutas[i])
+}
